423f42ad9b99d0b8cf6ff7b615f108ccc0bb7796
mahmoud ahmed <medoelmatry8@gmail.com>
2024-05-21 19:14:34 +0300

    Reapply "Add Quiz Page"
    This reverts commit e720ad5c8a0611b3bb31f83a440431adb5d4930f.

---
+++ E-LearningProject/wwwroot/js/frontendques.js
@@ -0,0 +1,102 @@
+let questions=[ //frontend
+{ numb : 1,
+question:"Who is making the Web standards?",
+answer:"The World Wide Web Consortium",
+options:[
+"Mozilla",
+"The World Wide Web Consortium",
+"Google",
+"Microsoft"
+]
+},
+{ numb :2 ,
+question:"What does CSS stand for?",
+answer:"Cascading Style Sheets  ",
+options:[
+"Cascading Style Sheets  ",
+"Creative Style Sheets",
+"Computer Style Sheets",
+"Colorful Style Sheets"
+]
+},
+{ numb : 3,
+question:"Which HTML attribute is used to define inline styles?",
+answer:"style",
+options:[
+"styles",
+"class",
+"font",
+"style"
+]
+},
+{ numb : 4,
+question:"How do you insert a comment in a CSS file?",
+answer:"/* this is a comment */",
+options:[
+"/* this is a comment */",
+"// this is a comment",
+"// this is a comment //",
+"/// this is a comment ///"
+]
+},
+{ numb : 5,
+question:"Which character is used to indicate an end tag?",
+answer:"/",
+options:[
+"^",
+"/",
+"<",
+"*"
+]
+},
+{ numb : 6,
+question:"Which property is used to change the background color?",
+answer:"background-color",
+options:[
+"background-color",
+"color",
+"bk-color",
+"bgcolor"
+]
+},
+{ numb :7 ,
+question:"Which CSS property controls the text size?",
+answer:"font-size",
+options:[
+"font-size",
+"text-size",
+"text-style",
+"font-style"
+]
+},
+{ numb : 8,
+question:"Which property is used to change the left margin of an element?",
+answer:"margin-left",
+options:[
+"padding-left",
+"padding-right",
+"margin-left",
+"margin-left-center"
+]
+},
+{ numb :9 ,
+question:"Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+answer:"alt",
+options:[
+"alt",
+"src",
+"title",
+"longdesc"
+]
+},
+{ numb : 10,
+question:"What is the default value of the position property?",
+answer:"static",
+options:[
+"flexbox",
+"flex",
+"fixed",
+"static"
+]
+},
+];
---
+++ E-LearningProject/wwwroot/js/webques.js
@@ -0,0 +1,102 @@
+let questions=[  
+{ numb :1 ,
+question:"Social Network is",
+answer:"All of the above",
+options:[
+"A way for students to share ideas.",
+"Not email",
+"Related to web 2.0",
+"All of the above"
+]
+},
+{ numb :2,
+question:"What is a trackback",
+answer:"A type of backline",
+options:[
+"A social site",
+"A type of backline",
+"A spam method",
+"A method of getting fake visits to your site"
+]
+},
+{ numb :3 ,
+question:"A blog can be used to",
+answer:"Communication your news to the world",
+options:[
+"Communication your news to the world",
+"Just to send send e-mail to your friends",
+"To type my homework at home",
+"To burn my cd's"
+]
+},
+{ numb : 4,
+question:"Who invented the term web 2.0 ?",
+answer:"Tim O'Relly",
+options:[
+"Bill Gates",
+"Yahoo",
+"Google",
+"Tim O'Relly"
+]
+},
+{ numb :5 ,
+question:"What Web 2.0 tool is best for collaboration ?",
+answer:"Blogs",
+options:[
+"Blogs",
+"Wikis",
+"podcasts",
+"Glogs"
+]
+},
+{ numb :6 ,
+question:"Web 2.0 can best be described as a (n)",
+answer:"Approach to the internet in which users share thoughts . Strategies and open forum such as blogs,wikis and podcasts",
+options:[
+"Software package developed by Microsoft and available at discount to teachers",
+"Approach to the internet in which users share thoughts . Strategies and open forum such as blogs,wikis and podcasts",
+"Variation of the internet created by Al Gore in 1996 ",
+"Faster internet for big business"
+]
+},
+{ numb : 7,
+question:"Which among these is not a Web browser?",
+answer:"www",
+options:[
+"www",
+"Edge",
+"opera",
+"google"
+]
+},
+{ numb : 8,
+question:"When did the term web 2.0 become popular?",
+answer:"2004",
+options:[
+"2004",
+"1994",
+"2008",
+"1990"
+]
+},
+{ numb :9 ,
+question:"When did the term web 2.0 become popular?",
+answer:"They get better as more people use it",
+options:[
+"They get better as more people use it",
+"Multiple users schedule their time to use it one by one",
+"They provide users with content rather than asking users to create it",
+"Web 2.0 application refers to a new version of a web 1.0 application"
+]
+},
+{ numb : 10,
+question:"What is the key characteristic of web 2.0?",
+answer:"Sharing & openess",
+options:[
+"Better design",
+"Sharing & openess",
+"Better sound",
+"User generated content"
+]
+},
+];
